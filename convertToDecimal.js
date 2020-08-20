c65848c9667b6f8e8c4d293432e9f20964f91185
Daniela Duarte <falecom@danieladuarte.com.br>
2020-08-19 21:34:19 -0300

    style: change indent

--- convertToDecimal.js
+++ convertToDecimal.js
@@ -34,19 +34,21 @@ const mappingXSystemForRomanSystem = [
         roman: 'M',
         value: 1000
     }
-]
+];
 
 function convertToDecimal(value) {
     const separator = value.split(" ");
-     let result = 0
-    for(let i = 0; i < separator.length-1; i++){
-        if(mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value < mappingXSystemForRomanSystem.find(e => e.name === separator[i+1]).value){
-           result -= mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value;
 
-        }else {
-        result += mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value;
-       }
+    let result = 0
+
+    for (let i = 0; i < separator.length - 1; i++) {
+        if (mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value < mappingXSystemForRomanSystem.find(e => e.name === separator[i + 1]).value) {
+            result -= mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value;
+
+        } else {
+            result += mappingXSystemForRomanSystem.find(e => e.name === separator[i]).value;
+        }
     }
-    return result + mappingXSystemForRomanSystem.find(e => e.name === separator[separator.length-1]).value
-}
 
+    return result + mappingXSystemForRomanSystem.find(e => e.name === separator[separator.length - 1]).value
+}
